145826a178f54f4845f7e90efe0886a909c9e703
hi there its me shamil <148104756+shamil-79@users.noreply.github.com>
2024-01-08 14:01:26 +0530

    Add files via upload

---
+++ task2/task2/task 2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    public interface Shape
+    {
+        double Getarea();
+        
+    }
+   
+    public class Circle: Shape
+    {
+        double Radious { get; set; }
+        public Circle(int radious)
+        {
+            Radious = radious;
+        }
+        public double Getarea()
+        {
+            return Math.PI * Radious * Radious;
+        }
+    }
+    public class Rectangle: Shape
+    {
+        int Height { get; set;}
+        int Width { get; set;}
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public double Getarea() 
+        { 
+            return Height * Width; 
+        }
+
+    }
+    public class Square : Shape
+    {
+        int Meter;
+        public Square(int meter)
+        {
+            Meter = meter;
+        }
+        public double Getarea()
+        {
+            return Meter * Meter;
+        }
+
+    }
+    public class Areafinder
+    {
+        public static void Main(string[] args)
+        {
+            Circle circle= new Circle(4);
+            Console.WriteLine(circle.Getarea());
+            Rectangle rectangle = new Rectangle(6, 3);
+            Console.WriteLine(rectangle.Getarea());
+            Square square = new Square(4);
+            Console.WriteLine(square.Getarea());
+
+
+        }
+    }
+}
